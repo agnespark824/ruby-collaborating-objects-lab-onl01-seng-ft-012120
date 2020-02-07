e2edb2b95a998a74bf948dd7055c187c5961a52b
Agnes Park <agnespark824@gmail.com>
2020-02-07 05:03:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,17 +6,11 @@ class MP3Importer
   end
   
   def files
-    Dir.entries(self.path).select do |f|
-      three_item_array = f.split(" - ")
-      two_item_array = three_item_array[2].split(".")
-      two_item_array[1] == "mp3"
+    Dir.entries(self.path) do |f|
+      Dr.glob(".mp3")
     end
   end
   
-    def files
-    @files ||= Dir.entries(@path).
-      Dir.glob("*.mp3")
-  end
 
   def import
     files.each { |f| Song.new_by_filename(f) }
